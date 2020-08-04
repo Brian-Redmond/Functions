f5fc4beb11039494f05f768d045e3458ca5b89f5
Brian-Redmond <67969029+Brian-Redmond@users.noreply.github.com>
2020-08-03 23:54:08 -0500

    Add files via upload

---
+++ Function.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+func minMax(array: [Int]) -> (min: Int, max: Int){
+    var currentMin = array [0]
+    var currentMax = array [0]
+    for value in array[1..<array.count] {
+        if value < currentMin {
+            currentMin = value
+        } else if value > currentMax {
+            currentMax = value
+}
+        
+}
+     return (currentMin, +currentMax)
+}
+let addition = minMax(array: [1 + 1])
+print("your sum is \(addition)")
